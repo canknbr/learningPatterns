11d0171de420badf0a5c0be0a08b798185d73343
Can Kanbur <cankanbur1@gmail.com>
2022-05-08 15:07:41 +0300

    .

--- index.js
+++ index.js
@@ -22,18 +22,36 @@ const user = {
   age: 29,
 };
 
+// const proxyUser = new Proxy(user, {
+//   get(target, prop) {
+//     console.log(`getting ${prop} and ${target[prop]}`);
+//     return target[prop];
+//   },
+//   set(target, prop, value) {
+//     console.log(
+//       `setting ${prop} to ${value} and modifying ${target.name} object`
+//     );
+//     return (target[prop] = value);
+//   },
+// });
+
+// console.log(proxyUser.name);
+// proxyUser.nationaly = 'turkish';
+
 const proxyUser = new Proxy(user, {
   get(target, prop) {
-    console.log(`getting ${prop} and ${target[prop]}`);
-    return target[prop];
+    if (!target[prop]) {
+      throw new Error(`${prop} is not a valid property`);
+    }
+    return Reflect.get(target, prop);
   },
   set(target, prop, value) {
-    console.log(
-      `setting ${prop} to ${value} and modifying ${target.name} object`
-    );
-    return (target[prop] = value);
+    if (prop == 'age' && typeof value !== 'number' && value < 0) {
+      throw new Error(`${prop} is not a valid property`);
+    }
+    return Reflect.set(target, prop, value);
   },
 });
 
-console.log(proxyUser.name);
-proxyUser.nationaly = 'turkish';
+let usr = proxyUser.name;
+console.log(usr);
